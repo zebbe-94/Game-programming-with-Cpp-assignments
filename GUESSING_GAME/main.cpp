368d81581e21e200948230ffcd6ee47e14f7806b
zebbe94 <63231705+zebbe-94@users.noreply.github.com>
2022-02-15 15:37:12 +0100

    Added ability to restart and handling of faulty input

--- GUESSING_GAME/main.cpp
+++ GUESSING_GAME/main.cpp
@@ -4,15 +4,31 @@ using namespace std;
 
 int main() {
     const int MAX_NUMBER = 10;
-    int random_number = rand() % (MAX_NUMBER + 1);
-    int n;
+    const int MIN_NUMBER = 0;
+    bool run_game = true;
+    srand (time(NULL));
 
-    cout << "I have a number between 0 and " << MAX_NUMBER << endl;
-    while(random_number != n){
-        cout << "Guess the number: " << endl;
-        cin >> n;
+    while(run_game){
+        int random_number = rand() % (MAX_NUMBER + 1) + MIN_NUMBER;
+        int n = INT_MIN;
+        cout << "I have a number between " << MIN_NUMBER << " and " << MAX_NUMBER << endl;
+        while(random_number != n){
+            cout << "Guess the number: " << endl;
+            cin >> n;
+            while(n < MIN_NUMBER || n > MAX_NUMBER){
+                cout << "Number is not within range, try again:  " << endl;
+                cin >> n;
+            }
+        }
+        cout << "You guessed the number!" << endl;
+
+        cout << "Would you like to play again? y/n" << endl;
+        char answer = ' ';
+        while (answer != 'y' && answer != 'n'){
+            cin >> answer;
+        }
+        run_game = answer == 'y' ? true : false;
     }
-    cout << "You guessed the number!" << endl;
 
     return 0;
 }
